36b46eb6137673b86b32b8b19c6d89dd9e61a9e5
Seungwon Park <psw14041@gmail.com>
2016-08-17 01:52:07 +0900

    sh 포기
    그런데 py도 잘 안돌아가고 있음

--- getnumber.py
+++ getnumber.py
@@ -3,23 +3,25 @@
 
 num = [0]
 import urllib2
-from sys import argv
-script, lottoNo = argv
-response = urllib2.urlopen('http://nlotto.co.kr/lotto645Confirm.do?method=byWin&drwNo=' + str(lottoNo))
+#from sys import argv
+#script, lottoNo = argv
+#from time import sleep
+for lottoNo in range(639, 716):
+    response = urllib2.urlopen('http://nlotto.co.kr/lotto645Confirm.do?method=byWin&drwNo=' + str(lottoNo))
 
-html = response.read()
-html_win = html.split('<div class="lotto_win_number mt12">')[1]
-html_win = html_win.split('</div>')[0]
-html_win_core = html_win.split('ball_')
+    html = response.read()
+    html_win = html.split('<div class="lotto_win_number mt12">')[1]
+    html_win = html_win.split('</div>')[0]
+    html_win_core = html_win.split('ball_')
 
-html_date = html.split(')</span></h3>')[0];
-html_date = html_date.split('/> <span>(')[1];
+    html_date = html.split(')</span></h3>')[0];
+    html_date = html_date.split('/> <span>(')[1];
 
-f = open('data/' + str(lottoNo) + '.csv', 'a')
+    f = open('data/' + str(lottoNo) + '.csv', 'a')
 
-f.write(html_date);
-#print (html_date);
+    f.write(html_date);
+    #print (html_date);
 
-for i in range(1,8):
-    num.append(html_win_core[i].split('.png')[0])
-    f.write(',' + num[i])
+    for i in range(1,8):
+        num.append(html_win_core[i].split('.png')[0])
+        f.write(',' + num[i])
