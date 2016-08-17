67e14f1b5bbdbe27ec0335a49a73f9d14264c428
Seungwon Park <psw14041@gmail.com>
2016-08-17 19:03:30 +0900

    Resolved encoding prob

--- getnumber.py
+++ getnumber.py
@@ -8,13 +8,14 @@ script, lottoNo = argv
 
 response = urllib2.urlopen('http://nlotto.co.kr/lotto645Confirm.do?method=byWin&drwNo=' + str(lottoNo))
 
-html = response.read()
+html = unicode(response.read(), "euc-kr").encode("utf-8")
 html_win = html.split('<div class="lotto_win_number mt12">')[1]
 html_win = html_win.split('</div>')[0]
 html_win_core = html_win.split('ball_')
 
 html_date = html.split(')</span></h3>')[0];
 html_date = html_date.split('/> <span>(')[1];
+html_date = html_date.split(' 추첨')[0]
 
 f = open('data/' + str(lottoNo) + '.csv', 'w')
 
