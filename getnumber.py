534b75b75c15a2deff15921f9eab2cfb196edd5f
Seungwon Park <psw14041@gmail.com>
2016-08-17 19:03:40 +0900

    Removed semicolons

--- getnumber.py
+++ getnumber.py
@@ -13,14 +13,14 @@ html_win = html.split('<div class="lotto_win_number mt12">')[1]
 html_win = html_win.split('</div>')[0]
 html_win_core = html_win.split('ball_')
 
-html_date = html.split(')</span></h3>')[0];
-html_date = html_date.split('/> <span>(')[1];
+html_date = html.split(')</span></h3>')[0]
+html_date = html_date.split('/> <span>(')[1]
 html_date = html_date.split(' 추첨')[0]
 
 f = open('data/' + str(lottoNo) + '.csv', 'w')
 
-f.write(html_date);
-#print (html_date);
+f.write(html_date)
+#print (html_date)
 
 for i in range(1,8):
     num.append(html_win_core[i].split('.png')[0])
